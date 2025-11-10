911465d4cd4499b991481bfc746281ca37a7b4ad
unknown <gabriel.junqueira@cotefacil.com>
2025-11-10 15:55:21 -0300

    #251110-01: Finalizado caso de teste 20, adicionado 21 e 22 (incompleto)

--- cypress/e2e/test20-pesquisarProdutosELogar.cy.js
+++ cypress/e2e/test20-pesquisarProdutosELogar.cy.js
@@ -13,7 +13,6 @@ import {
   celular,
   produtos
 } from '../../cypress.env.json'
-import { faker } from '@faker-js/faker'
 
 describe('Teste 20 - Vai pesquisar por produtos e depois do login, verificar o carrinho', () => {
   it('Depois de adicionar os produtos no carrinho, vai fazer login e verificar que todos estão lá', () => {
@@ -38,8 +37,54 @@ describe('Teste 20 - Vai pesquisar por produtos e depois do login, verificar o c
     cy.get('[class="col-sm-4"]').eq(produto1.id).should('be.visible')
     cy.adicionarNoCarrinho(produto1.id, produto1.quantidade)
 
-    // TODO: Finalizar esse caso de teste
+    // Digita o nome do produto e clica no botão de pesquisa
+    cy.get('[id="search_product"]').should('be.visible').clear().type(produto2.titulo)
+    cy.get('[class="fa fa-search"]').should('be.visible').click()
+
+    // Verifica o título da página
+    cy.get('[class="title text-center"]').contains('Searched Products').should('be.visible')
+
+    // Verificar se o produto está visível e o adiciona ao carrinho
+    cy.get('[class="col-sm-4"]').should('be.visible')
+    cy.adicionarNoCarrinho(produto2.id, produto2.quantidade)
+
+    // Vai para a pagina do carrinho
+    cy.get('[href="/view_cart"]')
+      .first()
+      .should('be.visible')
+      .click()
+
+    // Verifica se os valores dentro do carrinho estão de acordo com o esperado
+    cy.get('[class="table-responsive cart_info"]')
+      .should('be.visible')
+      .and('contain', produto1.titulo)
+      .and('contain', produto2.titulo)
+    // Verifica o carrinho para cada produto
+    cy.verificaDadosQuantitativosCarrinho(produto1.id, produto1.quantidade, produto1.preco)
+    cy.verificaDadosQuantitativosCarrinho(produto2.id, produto2.quantidade, produto2.preco)
+
+    // Fazer login do usuário, entrando na página de login
+    cy.get('[href="/login"]').first().should('be.visible').click()
+
+    // Chama a função que registra o usuário
+    cy.registrarUsuarioCompleto(usuario, email_usuario, senha, nome, sobrenome, empresa, endereco, pais, estado, cidade, cep, celular)
+
+    // Vai para a pagina do carrinho
+    cy.get('[href="/view_cart"]')
+      .first()
+      .should('be.visible')
+      .click()
+
+    // Verifica se os valores dentro do carrinho continuam dentro do esperado
+    cy.get('[class="table-responsive cart_info"]')
+      .should('be.visible')
+      .and('contain', produto1.titulo)
+      .and('contain', produto2.titulo)
+    // Verifica o carrinho para cada produto
+    cy.verificaDadosQuantitativosCarrinho(produto1.id, produto1.quantidade, produto1.preco)
+    cy.verificaDadosQuantitativosCarrinho(produto2.id, produto2.quantidade, produto2.preco)
+
     // Remove o usuário
-    //cy.removerUsuario();
+    cy.removerUsuario();
   })
 })
